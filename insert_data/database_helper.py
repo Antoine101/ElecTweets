ed5d613fd37a766195902e922dd17265d8072a6d
antoputt <anthony.pierre1@utt.fr>
2022-06-07 15:49:32 +0200

    Debug de l'insert des tweets json
    Correction du fichier db_creation.sql
    table tweets : author_id en BIGINT et content en varchar(208)
    Ajout de l'insert des tweets en base

--- insert_data/database_helper.py
+++ insert_data/database_helper.py
@@ -1,7 +1,13 @@
 import csv
+import string
 import pandas as pd
+import numpy as np
+from pathlib import Path
+from pandas import json_normalize
 from sqlalchemy import create_engine
 import psycopg2
+from psycopg2.extensions import register_adapter
+psycopg2.extensions.register_adapter(np.int64, psycopg2._psycopg.AsIs)
 
 # Create a connection to the database
 conn = psycopg2.connect(dbname="twitter_elections", user="admin", host="database", password="password")
@@ -139,6 +145,70 @@ contexte_elections = pd.read_sql_query("""
         FROM affiliation_elections
         """,con=engine, coerce_float=False)
 
+
+# Create field if null value
+def get_attribute(data, attribute, default_value):
+    if data.get(attribute) is None:
+        data[attribute] = default_value
+    return data
+
+# Remove punctuations
+def remove_punctuations(text):
+    for punctuation in string.punctuation:
+        text = text.replace(punctuation, '')
+    return text
+
+# Json do df with path data
+def json_to_df(json_file_path):
+    
+    data = pd.read_json(json_file_path)
+    if not (data.empty):
+        public_metric =json_normalize(data['public_metrics'])
+        data = pd.merge(data, public_metric, left_index=True, right_index=True)
+        data.drop(['public_metrics','conversation_id','context_annotations','geo','attachments','lang','source',
+                       'withheld','in_reply_to_user_id'], inplace=True, axis=1)
+        data.rename(columns={"id": "tweet_id", "created_at": "publication_date", "text":"content","like_count":"like_counts",
+                             "reply_count":"reply_counts","retweet_count":"retweet_counts","quote_count":"quote_counts"}, inplace = True)
+        
+        list_label = ['tweet_id', 'author_id', 'publication_date', 'like_counts', 'reply_counts', 'retweet_counts',
+                      'quote_counts','reply_settings', 'possibly_sensitive', 'label', 'polarity', 'content']
+        for i in list_label:
+            get_attribute(data, i, None)
+
+        data = data[list_label]
+        data['publication_date'] = data['publication_date'].dt.strftime('%Y-%m-%d')
+        data['content'] = data['content'].apply(remove_punctuations)
+        data = data.convert_dtypes()
+        data['possibly_sensitive'] = data['possibly_sensitive'].astype(str)
+
+    return data
+
+# Insert data from json to table tweets
+for file in Path("data/").glob("*.json"):
+    data_json = open(file,encoding="utf8")
+
+    tweet_data = json_to_df(data_json)
+    for i in range(0 ,len(tweet_data)):
+        values = (tweet_data['tweet_id'][i],tweet_data['author_id'][i],tweet_data['publication_date'][i],tweet_data['like_counts'][i],
+        tweet_data['reply_counts'][i],tweet_data['retweet_counts'][i],tweet_data['quote_counts'][i],tweet_data['reply_settings'][i],
+        tweet_data['possibly_sensitive'][i],tweet_data['label'][i],tweet_data['polarity'][i],tweet_data['content'][i])
+        cur.execute("""INSERT INTO tweets (
+                 tweet_id,
+                 author_id, 
+                 publication_date,
+                 like_counts,
+                 reply_counts,
+                 retweet_counts,
+                 quote_counts,
+                 reply_settings,
+                 possibly_sensitive,
+                 label,
+                 polarity,
+                 content
+                 ) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)""", values)
+        conn.commit()
+
+print('Data have been exported to PostGreSql Database')
 
 # Closing of of cursor and connection
 cur.close()
